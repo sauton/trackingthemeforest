eeeba7509b25647e64b4eac63e053ac1275af032
thang <thangpd@solazu.com>
2018-06-10 14:26:10 +0700

    ok csv

--- cURL.php
+++ cURL.php
@@ -11,43 +11,85 @@ class cURLtheme
 
     private $url_source = 'https://themeforest.net/category';
     public $numrank = 5;
-    private $arr_cat = [];
+    public $arr_cat = [];
+    private $dom;
 
     public function __construct()
     {
-
+        $this->dom = new DOMDocument();
+        $this->getUrlCategory();
     }
 
-
-    private function domHTML($url)
+    public function getUrlThemeforest()
     {
-        $dom = new DOMDocument();
-        libxml_use_internal_errors(true);
-        $dom->loadHTML($this->curl_theme($url));
-        return $dom;
+        return $this->url_source;
     }
 
-    public function gettoprank($arr_nitches = ['/wordpress/blog-magazine'], $top = 5, $filter = 'sales')
+    private
+    function domHTML($url)
+    {
+        libxml_use_internal_errors(true);
+        $this->dom->loadHTML($this->curl_theme($url));
+        return $this->dom;
+    }
+
+    public
+    function gettoprank($arr_nitches = [], $top = 5, $filter = 'sales')
     {
         $arr = [];
 
         $sort_by = '?sort=' . $filter;
-        if (!empty($arr_nitches)) {
-            $arr_nitches = $this->getUrlCategory();
+        if (empty($arr_nitches)) {
+            $arr_nitches = $this->arr_cat;
         }
         foreach ($arr_nitches as $item) {
+//            print_r($this->url_source . $item . $sort_by);
 
-            $dom = $this->domHTML($this->url_source . $item . $sort_by);
-            foreach ($dom->getElementsByTagName('a') as $link) {
-                # Show the <a href>
-                print_r($link->getAttribute('href'));
+            $this->domHTML($this->url_source . $item . $sort_by);
+
+            $url_arr = explode('/', $item);
+            $url_arr = $url_arr[count($url_arr) - 1];
+
+            $i = 1;
+            foreach ($this->dom->getElementsByTagName('a') as $link) {
+                if (preg_match('#.*s_rank=' . $i . '#', $link->getAttribute('href')) && $i <= $top) {
+                    $i++;
+                    $arr[$url_arr][] = $link->getAttribute('href');
+                }
             }
         }
         return $arr;
-
     }
 
-    public function getUrlCategory($url = '')
+    public function getDetailTheme($url)
+    {
+        $curl = $this->curl_theme($url);
+
+
+        $curl = trim(preg_replace('/\s\s+/', ' ', $curl));
+        $curl = trim(preg_replace('#<span class="sidebar-stats__icon"><i class="e-icon -icon-cart"></i></span>#', '', $curl));
+        $curl = preg_replace('#href="#', 'href="https://themeforest.net', $curl);
+        preg_match('#is-hidden-phone">(.*?)</h1>#', $curl, $title);
+        preg_match('#js-purchase-price">(.*?)</span>#', $curl, $price);
+        preg_match('#sidebar-stats__number">(.*?)</strong>#', $curl, $sale);
+        preg_match('#<a\srel="author".*href="(.*)">(.*)</a>#', $curl, $author);
+
+
+        return array($title[1],$url, $author[2], $author[1], $sale[1], $price[1]);
+
+        /*echo '<br>';
+        print_r($author);
+        echo '<br>';
+        print_r($sale);
+        echo '<br>';
+        print_r($price);
+        echo '<br>';
+        print_r($title);
+        echo '<br>';*/
+    }
+
+    public
+    function getUrlCategory($url = '')
     {
         if (empty($url)) {
             $url = $this->url_source;
@@ -56,18 +98,18 @@ class cURLtheme
         # Iterate over all the <a> tags
         foreach ($dom->getElementsByTagName('a') as $link) {
             # Show the <a href>
-            if (preg_match('#^\/category\/[a-z]+[\/]#', $link->getAttribute('href')) && $link->textContent != 'All Items') {
+            if (preg_match('#^\/category\/.[a-z]#', $link->getAttribute('href')) && $link->textContent != 'All Items') {
 
-                $this->arr_cat[] = substr($link->getAttribute('href'), 9);
+                $this->arr_cat[substr($link->getAttribute('href'), 9)] = $link->textContent;
 
             }
         }
 
-        return $this->arr_cat;
 
     }
 
-    private function convertUrltoArray($arr)
+    private
+    function convertUrltoArray($arr)
     {
         foreach ($arr as $item) {
             $path = $item;
@@ -88,7 +130,8 @@ class cURLtheme
         }
     }
 
-    private function curl_theme($url)
+    private
+    function curl_theme($url)
     {
 
         if (!$res = $this->getUrlContent($url)) {
@@ -97,7 +140,8 @@ class cURLtheme
         return $res;
     }
 
-    private function getUrlContent($url)
+    private
+    function getUrlContent($url)
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Track Theme Forest</title>
+</head>
+<body>
 <?php
 /**
  * Created by PhpStorm.
@@ -5,19 +15,137 @@
  * Date: 6/9/18
  * Time: 9:49 AM
  */
+
+
 require('cURL.php');
-require('dom/simple_html_dom.php');
+$rank = new cURLtheme();
 
-$cate = new cURLtheme();
-$res = $cate->getUrlCategory();
-
+//page one get url cat
+$res = $rank->arr_cat;
 echo '<pre>';
 //print_r($res);
 echo '</pre>';
 
-$rank = new cURLtheme();
-$res_rank = $rank->gettoprank($rank->numrank);
+$sortby = '';
+$top = '';
+$themes = [];
+if (!empty($_GET['top'])) {
+    $top = $_GET['top'];
+}
+if (!empty($_GET['sort'])) {
+    $sortby = $_GET['sort'];
+}
+if (!empty($_GET['themes'])) {
+    $themes = $_GET['themes'];
+}
+
+
+//page 2 get detail
+$output = '';
+$temp = '';
+foreach ($rank->arr_cat as $key => $value) {
+    $exp = explode('/', $key);
+    if ($temp != $exp[1]) {
+        $temp = $exp[1];
+        $output .= '<tr>';
+        $output .= '<td><h2><label><input type="checkbox" name="themes" value="' . $key . '">' . $value . '</label></h2></td>';
+        $output .= '</tr>';
+
+    } else {
+        $output .= '<td><label><input type="checkbox" name="themes[]" value="' . $key . '">' . $value . '</label></td>';
+    }
+}
+?>
+
+<form action="" method="get">
+    <table>
+        <input type="submit">
+        <br>
+        <select name="sort" id="">
+            <option value="sales">sales</option>
+            <option value="rating">rating</option>
+        </select>
+        <br>
+        <label for="">top<input type="text" name="top"></label>
+        <?php echo $output; ?>
+    </table>
+</form><?php
+
+
+$res_rank_url = $rank->gettoprank($themes, 5, $sortby);
 
 echo '<pre>';
-print_r($res_rank);
+//print_r($res_rank);
 echo '</pre>';
+
+
+$array_rank_url_detail = [];
+foreach ($res_rank_url as $key => $value) {
+    foreach ($value as $link_detail) {
+        $array_rank_url_detail[$key][] = $rank->getDetailTheme($link_detail);
+    }
+}
+echo '<pre>';
+print_r($array_rank_url_detail);
+echo '</pre>';
+
+?>
+
+<table>
+    <tr>
+        <td>Nitches</td>
+        <td>Name</td>
+        <td>Author</td>
+        <td>Sales</td>
+        <td>Price</td>
+    </tr>
+
+    <?php
+    foreach ($array_rank_url_detail as $key => $val) {
+        echo '<td>' . $key . '</td>';
+        foreach ($val as $v) {
+            echo '<tr>';
+            echo '<td></td>';
+            echo '<td><a href="' . $v[1] . '">' . $v[0] . '</a></td>';
+            echo '<td><a href="' . $v[3] . '">' . $v[2] . '</a></td>';
+            echo '<td>' . $v[4] . '</td>';
+            echo '<td>' . $v[5] . '</td>';
+            echo '</tr>';
+
+        }
+    }
+    ?>
+</table>
+<?php
+/*$curl= '<h2 class="t-heading -size-s">
+          <a rel="author" class="t-link -color-dark -decoration-none" href="/user/tielabs">TieLabs</a>
+      </h2>';
+$curl = trim(preg_replace('/\s\s+/', ' ', $curl));
+$curl = preg_replace('#href="#', 'href="https://themeforest.net', $curl);
+preg_match('#<a\srel="author".*>(.*)</a>#', $curl, $price);
+print_r($price);*/
+
+function array2csv(array $array, $sortby)
+{
+    if (count($array) == 0) {
+        return null;
+    }
+    ob_start();
+    $df = fopen("" . date('Y-m-d') . $sortby . ".csv", 'w');
+    foreach ($array as $key => $value) {
+        fputcsv($df, array($key));
+        foreach ($value as $row) {
+            fputcsv($df, $row);
+        }
+    }
+    fclose($df);
+    return ob_get_clean();
+}
+
+array2csv($array_rank_url_detail, $sortby);
+?>
+
+
+</body>
+</html>
+
